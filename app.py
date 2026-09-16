56ee66f384a2ca1552011dd48798ab6274608078
Luiz Isgroi <luiz.g.isgroi78@gmail.com>
2026-09-16 19:52:34 -0300

    Refactor database connection

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
-import os
+from database import get_connection
 
-import psycopg
-from dotenv import load_dotenv
 
-load_dotenv()
-
-with psycopg.connect(
-    host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT"),
-    dbname=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-) as connection:
+with get_connection() as connection:
     with connection.cursor() as cursor:
-        cursor.execute("SELECT id, name, email, created_at FROM students;")
+        cursor.execute(
+            """
+            SELECT id, name, email, created_at
+            FROM students
+            ORDER BY id;
+            """
+        )
+
         students = cursor.fetchall()
+
 
 for student in students:
     print(student)
--- database.py
+++ database.py
@@ -5,14 +5,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-connection = psycopg.connect(
-    host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT"),
-    dbname=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-)
 
-print("Database connection successful.")
-
-connection.close()
+def get_connection():
+    return psycopg.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+    )
+    
